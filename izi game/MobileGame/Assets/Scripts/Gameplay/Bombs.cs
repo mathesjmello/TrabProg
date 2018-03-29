7651908085f2322847d63421d7cda3d03b875060
Unknown <junqueirademello@gmail.com>
2018-03-28 22:22:01 -0300

    livrando de algumas bostas

--- izi game/MobileGame/Assets/Scripts/Gameplay/Bombs.cs
+++ izi game/MobileGame/Assets/Scripts/Gameplay/Bombs.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Bombs : MonoBehaviour
 {
 	public string ThisLevel;
+	public AudioSource SFX;
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag.Equals("Player"))
 		{
+			SFX.Play();
 			MyLoading.Reset(ThisLevel);
 		}
 	}
